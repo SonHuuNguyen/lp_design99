a20c9c22ef93fe24f9da1b5016887132d2d52055
SonHuuNguyen <nguyensonute@gmail.com>
2017-03-05 22:34:21 +0700

    update landing page 3rd

--- index.php
+++ index.php
@@ -92,37 +92,113 @@
 			</a>
 		</div>
 		<!-- Form for quote -->
-		<div id="quote-container"><div id="quote-form" class="col-md-4 col-sm-6 col-xs-12 col-md-offset-4 col-sm-offset-2">
-					<div id="form-brand">
-						<h2>Lower My Rate</h2>
-					</div>
-					<form>
-						<div class="form-group">
-						<br>
-							<input type="text" id="full-name" placeholder="Full-name">
-						</div>
-						<div class="form-group">
-							<input type="email" id="email" placeholder="Email">
-						</div>
-						<div class="form-group">
-							<input type="text" id="company-name" placeholder="Company name">
-						</div>
-						<div class="form-group">
-							<input type="text" id="phone-number" placeholder="Phone number">
-						</div>
-						<div class="row">
-						<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3 col-sm-offset-3"><button type="submit" class="btn btn-block" id="quote-submit"><h3>Submit</h3></button></div>
-							
-						</div>
-					</form>
-				</div></div>
+		<div id="fadded-screen">
+		</div>
+		<div class="row" id="quote-container">	<div id="quote-form" class="col-md-4 col-sm-6 col-xs-12 col-md-offset-4 col-sm-offset-2">
+			<div id="form-brand">
+				<h2>Lower My Rate</h2>
+			</div>
+			<form>
+				<div class="form-group">
+					<br>
+					<input type="text" id="full-name" placeholder="Full-name">
+				</div>
+				<div class="form-group">
+					<input type="email" id="email" placeholder="Email">
+				</div>
+				<div class="form-group">
+					<input type="text" id="company-name" placeholder="Company name">
+				</div>
+				<div class="form-group">
+					<input type="text" id="phone-number" placeholder="Phone number">
+				</div>
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3 col-sm-offset-3"><button type="submit" class="btn btn-block" id="quote-submit"><h3>Submit</h3></button></div>
+				</div>
+			</form>
+		</div></div>
 		<!-- End of form for quote -->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-3 col-sm-3 col-xs-12">
-					
+		
+
+		<div class="sect sect-img1">
+			<div id="our-service">
+				<h1 id="our-service-caption">Our service </h1>
+				<span class="glyphicon glyphicon-cog" id="icon-service1"></span> 
+				<span class="glyphicon glyphicon-cog" id="icon-service2"></span> 
+			</div>
+		</div>
+		<div class="container" id="main-container">
+			<div class="row" id="inner-main">
+				<div class="col-xs-12 col-sm-3 col-md-3 service-item" id="service-item1">
+					<h2>Credit Card Terminal</h2>
+					<h3>Chip card | Apple pay</h3>
+					<img src="img/service-item1.png" alt="credit-card-terminal">
+					<div class="overlay">
+						<p>Credit Card Terminal</p>
+						<button>View more</button>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 service-item" id="service-item2">
+					<h2>Point of Sale Systems</h2>
+					<h3>Retail | Restaurant</h3>
+					<img src="img/service-item2.png" alt="credit-card-terminal">
+					<div class="overlay">
+						<p>Point of Sale Systems</p>
+						<button>View more</button>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 service-item" id="service-item3">
+					<h2>E-Commere</h2>
+					<h3>E-Commerce Websites</h3>
+					<img src="img/service-item3.png" alt="credit-card-terminal">
+					<div class="overlay">
+						<p>E-Commere</p>
+						<button>View more</button>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 service-item" id="service-item4">
+					<h2>Mobile</h2>
+					<h3>Inventory | Invoicing</h3>
+
+					<img src="img/service-item4.png" alt="credit-card-terminal">
+					<div class="overlay">
+					<p>Mobile</p>
+						<button>View more</button>
+					</div>
 				</div>
 			</div>
 		</div>
+		<div class="sect sect-img2">
+		</div>
+		<div class="container" id="why-us">
+			<div class="row" id="inner-why-us">
+				<div class="col-xs-12 col-sm-5 col-md-5" id="whyus-left">
+					<div id="icon-whyus">
+						<img class="img-circle" src="img/whyus.jpg" alt="why-us">
+					</div>
+					<h1>Why partner wiht us?</h1>
+				</div>
+				<div class="col-xs-12 col-sm-7 col-md-7" id="whyus-right">	
+					<div class="row why-row"  id="why-1">
+						<div class="col-xs-12 col-sm-2 col-md-2 whyus-right-icon">
+						</div>
+						<div class="col-xs-12 col-sm-8 col-md-8 whyus-right-content">
+							<h1>Customer Service</h1>
+							<h4>Knowledgeable account representatives deliver personal service</h4>
+						</div>
+					</div>
+					<div class="row why-row"  id="why-2">
+						<div class="col-xs-12 col-sm-2 col-md-2 whyus-right-icon">
+						</div>
+						<div class="col-xs-12 col-sm-8 col-md-8 whyus-right-content">
+							<h1>Customer Service</h1>
+							<h4>Knowledgeable account representatives deliver personal service</h4>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
 	</body>
 	</html>
--- js/lp.js
+++ js/lp.js
@@ -1,33 +1,46 @@
 $(document).ready(function(){
+$('#fadded-screen').hide();
 $('#quote-container').hide();
 $(".fadeIn").hide();
 $(".fadeIn").fadeIn(3000);
 
-// var under_input = $(".form-group").find('div');
+
 
 $('.form-group input').focus(function(){
 	$('.form-group').removeClass('on_focus');
 	$(this).parent('.form-group').addClass('on_focus');
 
-})
+});
+
 $('.get-quote').click(function() {
 var carousel_img = $(".carousel-inner .active").find("img").height();
 $('#quote-container').css("height", carousel_img+50);
 $("#quote-form").animate({
         top: '-=50px',
     });
+$('#fadded-screen').show();
 $('#quote-container').show();
 });
 
-$('#quote-container').click(function(event) {
-	if(event.target.id == "quote-container"){
+$('#fadded-screen').click(function(event) {
+	if(event.target.id == "fadded-screen"){
 		$("#quote-form").css({
 		top: '+=50px'
 	});
 		$(this).hide();
+		$('#quote-container').hide();
 	}
+});
+$('#our-service').hover(function(event){
+	
+	$(this).find('h1').animate({fontSize: '3em'}, "fast");	
+});
+$('#our-service').mouseleave(function(event){
+
+		$(this).find('h1').animate({fontSize: '2.5em'}, "fast");
 	
 });
+
 
 
 });
